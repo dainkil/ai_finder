220f51cada8dad475fbb7f6b3dd707b340d14cfe
woojinHan <hwjin204@gmail.com>
2025-08-14 03:31:03 +0900

    simple refactoring

---
+++ srcs/filter_comments.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from srcs.config import API_KEY
+from libs.youtube_data import CommentCrawler, CommentFilter
+from pathlib import Path
+
+
+list_of_video_ids = []
+path_to_db = Path("..") / "dbs"
+
+
+for video_id in list_of_video_ids:
+
+    csv_filename = path_to_db / f"youtube_comments_{video_id}.csv"
+    df = pd.read_csv(csv_filename)
+    comment_filter = CommentFilter(df)
+    filtered_df = comment_filter.apply_rule_based_filter()
+
+    comment_filter.save_flagged_comments(
+        path_to_db / "flagged_spam_comments_{video_id}.csv"
+    )
---
+++ srcs/get_comments.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from srcs.config import API_KEY
+from libs.youtube_data import CommentCrawler, CommentFilter
+from pathlib import Path
+
+
+list_of_video_ids = []
+path_to_db = Path("..") / "dbs"
+
+
+for video_id in list_of_video_ids:
+    crawler = CommentCrawler(API_KEY)
+    print(f"[INFO] Fetching comments for video: {video_id} ...")
+    comments = crawler.fetch_comments(video_id)
+
+    if not comments:
+        print("[ERROR] No comments fetched. Exiting.")
+        continue
+
+    csv_filename = path_to_db / f"youtube_comments_{video_id}.csv"
+    crawler.to_csv(comments, csv_filename)
+
+    print("[INFO] Done.")
